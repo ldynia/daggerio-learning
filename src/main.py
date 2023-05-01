45ad3fbe65a0bc16e64f83ea7773a3f49a907a96
ldynia <l_dynowski@yahoo.com>
2023-05-01 13:42:39 +0200

    refactoring

--- src/main.py
+++ src/main.py
@@ -2,26 +2,33 @@
 
 import sys
 import anyio
+
 import dagger
+
+
+IMAGE = "python"
+TAG = "3.10-slim-buster"
 
 
 async def provision_python():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
-        container = client.container().from_("python:3.10-slim-buster")
+        container = client.container().from_(f"{IMAGE}:{TAG}")
         container = container.with_exec(["python", "-V"])
+
         py_version = await container.stdout()
-    
-    print(f"{py_version}")
+
+    print(f">>> {py_version}")
 
 
 async def provision_os():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
-        container = client.container().from_("python:3.10-slim-buster")
+        container = client.container().from_(f"{IMAGE}:{TAG}")
         container = container.with_exec(["apt-get", "update"])
         container = container.with_exec(["apt-get", "install", "-y", "vim"])
+
         exit_code = await container.exit_code()
-    
-    print(f"Program status code: {exit_code}")
+
+    print(f">>> Program exit code: {exit_code}")
 
 
 if __name__ == "__main__":
